cc16da2178700c6f2230fbbc899faa7dee757c2f
A. Novikov <novikovav2@gmail.com>
2017-12-10 05:45:34 +0400

    Correct code

--- svc.rb
+++ svc.rb
@@ -1,21 +1,18 @@
 # Author: A. Novikov
 #
-# Usefull link:
+# Useful link:
 # https://www.ibm.com/support/knowledgecenter/STLM6B_7.8.0/com.ibm.storwize.v3500.780.doc/svc_clusterstartstatswin_20qm0u.html
 #
-
 
 dir = '../iostat/' # Folder for statistics files
 influxdb_host = 'localhost' # InfluxDB host
 influxdb_db = 'svc' # InfluxDB database
-
 
 require 'nokogiri'
 require 'date'
 require 'time'
 require 'influxdb'
 require 'fileutils'
-
 
 @influxdb = InfluxDB::Client.new influxdb_db, host: influxdb_host
 
@@ -45,68 +42,17 @@ def vdisk_processing(xml)
     vdisk.attributes.each do |attr, value|
       next if (attr.to_s == 'idx') || (attr.to_s == 'id')
       data = {
-          values: { value: value.value.to_i },
-          timestamp: @timestamp,
-          tags: {
-              cluster: @cluster,
-              cluster_id: @cluster_id,
-              vdisk_id: vdisk.attr('idx').to_i,
-              vdisk: vdisk.attr('id')
-          }
+        values: { value: value.value.to_i },
+        timestamp: @timestamp,
+        tags: {
+          cluster: @cluster,
+          cluster_id: @cluster_id,
+          vdisk_id: vdisk.attr('idx').to_i,
+          vdisk: vdisk.attr('id')
+        }
       }
       @influxdb.write_point('vdisk_' + attr, data)
     end
-
-    # idx = vdisk.attr('idx')
-    # id = vdisk.attr('id')
-    # ro = vdisk.attr('ro')
-    # wo = vdisk.attr('wo')
-    # rb = vdisk.attr('rb')
-    # wb = vdisk.attr('wb')
-    # rl = vdisk.attr('rl')
-    # wl = vdisk.attr('wl')
-    # rlw = vdisk.attr('rlw')
-    # wlw = vdisk.attr('wlw')
-    # gwo = vdisk.attr('gwo')
-    # gwot = vdisk.attr('gwot')
-    # gws = vdisk.attr('gws')
-    # gwl = vdisk.attr('gwl')
-    #
-    # common_data = {
-    #   timestamp: @timestamp,
-    #   tags: {
-    #     cluster: @cluster,
-    #     cluster_id: @cluster_id,
-    #     vdisk_id: idx,
-    #     vdisk: id
-    #   }
-    # }
-    #
-    # ro_data = { values: { value: ro } }.merge common_data
-    # wo_data = { values: { value: wo } }.merge common_data
-    # rb_data = { values: { value: rb } }.merge common_data
-    # wb_data = { values: { value: wb } }.merge common_data
-    # rl_data = { values: { value: rl } }.merge common_data
-    # wl_data = { values: { value: wl } }.merge common_data
-    # rlw_data = { values: { value: rlw } }.merge common_data
-    # wlw_data = { values: { value: wlw } }.merge common_data
-    # gwo_data = { values: { value: gwo } }.merge common_data
-    # gwot_data = { values: { value: gwot } }.merge common_data
-    # gws_data = { values: { value: gws } }.merge common_data
-    # gwl_data = { values: { value: gwl } }.merge common_data
-    #
-    # @influxdb.write_point('vdisk_ro', ro_data)
-    # @influxdb.write_point('vdisk_wo', wo_data)
-    # @influxdb.write_point('vdisk_rb', rb_data)
-    # @influxdb.write_point('vdisk_wb', wb_data)
-    # @influxdb.write_point('vdisk_rl', rl_data)
-    # @influxdb.write_point('vdisk_wl', wl_data)
-    # @influxdb.write_point('vdisk_rlw', rlw_data)
-    # @influxdb.write_point('vdisk_wlw', wlw_data)
-    # @influxdb.write_point('vdisk_gwo', gwo_data)
-    # @influxdb.write_point('vdisk_gwot', gwot_data)
-    # @influxdb.write_point('vdisk_gws', gws_data)
-    # @influxdb.write_point('vdisk_gwl', gwl_data)
   end
 end
 
@@ -133,5 +79,5 @@ Dir.foreach(dir) do |file|
 
   f.close
   FileUtils.cp(dir + file, dir + 'old/')
-  FileUtils.remove(dir + file, :force => true)
+  FileUtils.remove(dir + file, force: true)
 end
