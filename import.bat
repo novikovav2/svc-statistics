7a921fe3fff89846feb579faf50d65e0b2d7696c
Aleksandr Novikov <novikovav2@gmail.com>
2018-02-03 20:55:45 +0400

    Edit README

--- import.bat
+++
@@ -1,2 +0,0 @@
-pscp.exe -unsafe -l svc_user -pw ******* 192.168.10.10:/dumps/iostats/* C:\Users\novikovav2\Documents\svc\
-plink -l svc_user -pw ******* 192.168.10.10 "cleardumps -prefix /dumps/iostats"
